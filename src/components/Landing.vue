85e5dab0b2d1dec7aa114d4861b3f7a200679e3a
Graham Nessler <grahamnessler@IRXWMKCHV2J.local>
2018-04-28 15:07:31 -0500

    Simplify code

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -5,14 +5,10 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'Landing',
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
 </style>
