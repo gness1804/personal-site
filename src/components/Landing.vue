742b990d21b03d1d3ec3205a3176eac89c52a4ed
Graham Nessler <grahamnessler@IRXWMKCHV2J.local>
2018-04-28 14:58:58 -0500

    Initial commit

---
+++ src/components/Landing.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  h1 Graham Nessler
+</template>
+
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
