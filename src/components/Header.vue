8e0d82e5326405605df0c78140018d2729aadb7d
Graham Nessler <grahamnessler@IRXWMKCHV2J.local>
2018-05-09 12:27:07 -0500

    Add linkedin and medium links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,6 +6,8 @@
         img(src="../assets/at.png" alt="Email me." v-on:click="openEmail" class="social-icon clickable-img")
         img(src="../assets/github.png" alt="Github logo." v-on:click="openGithub" class="social-icon clickable-img")
         img(src="../assets/twitter.png" alt="Twitter logo." v-on:click="openTwitter" class="social-icon clickable-img")
+        img(src="../assets/linkedin.png" alt="LinkedIn logo." v-on:click="openLinkedIn" class="social-icon clickable-img")
+        img(src="../assets/medium.png" alt="Medium logo." v-on:click="openMedium" class="social-icon clickable-img")
     nav
       ul(class="header-links")
         li
@@ -30,6 +32,12 @@ export default {
     },
     openGithub: function () {
       window.open('https://github.com/gness1804/');
+    },
+    openLinkedIn: function () {
+      window.open('https://www.linkedin.com/in/gness1804');
+    },
+    openMedium: function () {
+      window.open('https://medium.com/@gness1804/');
     },
     openTwitter: function () {
        window.open('https://twitter.com/gness1804/');
