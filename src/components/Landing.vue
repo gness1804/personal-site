113940a679a46fa0956dc724243f883645492115
gness1804 <graham.nessler@gmail.com>
2018-05-09 19:18:44 -0500

    Style landing

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -9,15 +9,3 @@ export default {
   name: 'Landing',
 }
 </script>
-
-<style scoped>
-  #landing {
-    margin: 0 20vw;
-  }
-
-  .splash-image {
-    height: 35rem;
-    margin-top: 60px;
-    width: 100%;
-  }
-</style>
