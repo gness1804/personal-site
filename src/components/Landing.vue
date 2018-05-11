5ca174364946b128dcac7ae51e4a369d210fda04
Graham Nessler <grahamnessler@IRXWMKCHV2J.local>
2018-05-11 13:39:29 -0500

    Add splash page text

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
   #landing
     img(src="../assets/graham-puck-splash.jpg" alt="My former dog and I." class="splash-image")
+    .landing-splash-text-container
+      p(class="main-text") Web Developer, Author, Traveler, and Cat Lover
 </template>
 
 
