47a88b04d408cac251c2b2ad56cd0af684ecbe21
Graham Nessler <grahamnessler@IRXWMKCHV2J.local>
2018-05-11 13:51:27 -0500

    Add sub text for landing pg

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -2,7 +2,8 @@
   #landing
     img(src="../assets/graham-puck-splash.jpg" alt="My former dog and I." class="splash-image")
     .landing-splash-text-container
-      p(class="main-text") Web Developer, Author, Traveler, and Cat Lover
+      p(class="main-text") Web Developer, Author, Traveler, and Pet Lover
+      p(class="sub-text") (and fan of the Oxford comma)
 </template>
 
 
