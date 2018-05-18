19fdbfde92ea7ff0fcc9bbcf1a928903db1102c3
Graham Nessler <grahamnessler@IRXWMKCHV2J.local>
2018-05-18 16:28:37 -0500

    Add github icon for code page

--- src/components/Code.vue
+++ src/components/Code.vue
@@ -1,11 +1,17 @@
 <template lang="pug">
   .code
     h2(class="code-heading") Stuff I built
+    img(src="../assets/github.png" alt="Github logo" class="big-github-img clickable-img" title="Github" v-on:click="openGithub")
 </template>
 
 <script>
 export default {
   name: 'Code',
+  methods: {
+    openGithub: function (): void {
+      window.open('https://github.com/gness1804/');
+    },
+  },
 };
 </script>
 
