39f4867545efa20f7300e9ec915a5610e401cbb5
Graham Nessler <grahamnessler@IRXWMKCHV2J.local>
2018-05-18 16:56:13 -0500

    Create projects file with empty project objects

---
+++ src/helpers/projects.js
@@ -0,0 +1,29 @@
+// @flow
+
+import { ProjectI } from '../types/interfaces/';
+
+const projects: ProjectI[] = [
+  {
+    name: '',
+    description: '',
+    href: '',
+  },
+  {
+    name: '',
+    description: '',
+    href: '',
+  },
+  {
+    name: '',
+    description: '',
+    href: '',
+  },
+  {
+    name: '',
+    description: '',
+    href: '',
+  },
+];
+
+export default projects;
+
