5f838ed7aac5ef2b868214f200bf46edb80905b5
Graham Nessler <grahamnessler@IRXWMKCHV2J.local>
2018-05-18 17:02:03 -0500

    Add flash shopper project info

--- src/helpers/projects.js
+++ src/helpers/projects.js
@@ -4,9 +4,9 @@ import { ProjectI } from '../types/interfaces/';
 
 const projects: ProjectI[] = [
   {
-    name: '',
-    description: '',
-    href: '',
+    name: 'Flash Shopper',
+    description: 'A grocery shopping and recipes app build in Vue.js, Firebase, and Pug. Integrates grocery shopping with recipes and allows user to connect to external sites such as Instacart and sort by aisle in a brick-and-mortar store.',
+    href: 'https://github.com/gness1804/flash-shopper-web-vue',
   },
   {
     name: '',
