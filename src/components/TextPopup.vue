52c07df7bab36fbefcad0276fb7068d6328a7b3f
gness1804 <graham.nessler@gmail.com>
2018-05-25 16:45:22 -0500

    Got basic logic for text popup

---
+++ src/components/TextPopup.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  .text-popup
+    p I am the text popup.
+</template>
+
+<script>
+
+export default {
+  name: 'TextPopup',
+};
+</script>
--- src/components/VariedDescription.vue
+++ src/components/VariedDescription.vue
@@ -1,14 +1,19 @@
 <template lang="pug">
   .varied-description
     p(class="varied-description-text") {{truncatedText}}
+    TextPopup
 </template>
 
 <script>
 // @flow
 import truncate from '../helpers/truncate';
+import TextPopup from './TextPopup.vue';
 
 export default {
   name: 'VariedDescription',
+  components: {
+    TextPopup,
+  },
   props: {
     text: {
       type: String,
