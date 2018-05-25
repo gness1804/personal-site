9c05f9050788fda341dd6de0aaf8db45f349f7bf
gness1804 <graham.nessler@gmail.com>
2018-05-25 16:32:48 -0500

    Finish with truncate function

--- src/helpers/truncate.js
+++ src/helpers/truncate.js
@@ -2,6 +2,13 @@
 
 import { TruncateI } from '../types/interfaces';
 
-const truncate: TruncateI = text => `${text.split(' ').slice(0, 50).join(' ')}...`;
+const truncate: TruncateI = (text) => {
+  const words: string[] = text.split(' ');
+  const maxWordLen: number = 50;
+  if (words.length <= maxWordLen) {
+    return text;
+  }
+  return `${words.slice(0, maxWordLen).join(' ')}...`;
+};
 
 export default truncate;
