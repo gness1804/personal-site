3133fb2260e6185414b3348363cbedb5176d7e09
gness1804 <graham.nessler@gmail.com>
2018-05-29 20:15:06 -0500

    Change title of MyGov

--- src/helpers/projects.js
+++ src/helpers/projects.js
@@ -30,7 +30,7 @@ const projects: ProjectI[] = [
   {
     id: 4,
     image: 'http://localhost:8080/assets/we-the-people.png',
-    name: 'MyGov (Formerly We The People) (with Ian Lancaster)',
+    name: 'MyGov (with Ian Lancaster)',
     description: 'MyGov is an application created in React Native that allows a user to track current Congressional bills and new laws. Using the Sunlight Congress API, the application brings in data on bills and laws including title, date introduced, date of last action, sponsor, current progress (in House Committee, passed by Senate, enacted...), and other data. The application won first prize in a code competition at Turing School in March 2017.',
     href: 'https://github.com/gness1804/we-the-people-native',
   },
