4e03912ffaaa50218c5cea96bb0a096b78b49e89
gness1804 <graham.nessler@gmail.com>
2018-05-31 19:49:04 -0500

    Add book cover and title

--- src/components/Writing.vue
+++ src/components/Writing.vue
@@ -1,6 +1,10 @@
 <template lang="pug">
   .writing
-    h2(class="writing-heading") Stuff I Have Written
+    h2(class="writing-heading") Stuff I Wrote
+    .book-container
+      a(href="https://www.amazon.com/Islandwide-Struggle-Freedom-Emancipation-Reenslavement-ebook/dp/B015ZTG2UM/ref=sr_1_1?ie=UTF8&qid=1446851797&sr=8-1&keywords=graham+nessler" target="_blank" title="Buy my book!") 
+        img(src="../assets/book-cover_.jpg" alt="The cover of my book." class="writing-book-cover-image")
+      p(class="writing-book-title") An Islandwide Struggle for Freedom: Revolution, Emancipation, and Reenslavement in Hispaniola, 1789-1809
 </template>
 
 <script>
