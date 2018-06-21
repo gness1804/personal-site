92638d2de0919d479e65e65516706157b94c078e
gness1804 <graham.nessler@gmail.com>
2018-06-20 20:30:34 -0500

    Break out icons to a mixin

--- src/components/Writing.vue
+++ src/components/Writing.vue
@@ -1,15 +1,12 @@
 <template lang="pug">
+  include mixins/icons
   .writing
     h2(class="writing-heading") Stuff I Wrote
     .book-container
       a(href="https://www.amazon.com/Islandwide-Struggle-Freedom-Emancipation-Reenslavement-ebook/dp/B015ZTG2UM/ref=sr_1_1?ie=UTF8&qid=1446851797&sr=8-1&keywords=graham+nessler" target="_blank" title="Buy my book!")
         img(src="../assets/book-cover_.jpg" alt="The cover of my book." class="writing-book-cover-image")
       p(class="writing-book-title") An Islandwide Struggle for Freedom: Revolution, Emancipation, and Reenslavement in Hispaniola, 1789-1809
-    .icons-container
-      a(href="https://www.uncpress.org/book/9781469626864/an-islandwide-struggle-for-freedom/" target="_blank")
-        img(src="../assets/unc-press.png" alt="UNC Press logo." class="icons-container-icon")
-      a(href="https://www.amazon.com/Islandwide-Struggle-Freedom-Emancipation-Reenslavement-ebook/dp/B015ZTG2UM/ref=sr_1_1?ie=UTF8&qid=1446851797&sr=8-1&keywords=graham+nessler" target="_blank")
-        img(src="../assets/amazon-logo.jpg" alt="Amazon logo." class="icons-container-icon")
+    +icons()
 </template>
 
 <script>
