a177b926076e7dda4142a0978e777d7af98fe73b
Graham Nessler <grahamnessler@IRXWMKCHV2J.local>
2018-06-22 15:38:23 -0500

    Add article mixin

--- src/components/Medium.vue
+++ src/components/Medium.vue
@@ -1,6 +1,11 @@
 <template lang="pug">
+  include mixins/article
   .medium-articles
     p(class="medium-articles-headline") Medium Articles
+    .articles-container
+      +article('hello', 'world')
+      +article('hello', 'world')
+      +article('hello', 'world')
 </template>
 
 <script>
