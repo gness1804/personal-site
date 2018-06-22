4ac0625ba3a37d9fb66e3e4424fe408cf019b689
Graham Nessler <grahamnessler@IRXWMKCHV2J.local>
2018-06-22 15:52:38 -0500

    Add Turing Medium story

--- src/components/Medium.vue
+++ src/components/Medium.vue
@@ -3,7 +3,7 @@
   .medium-articles
     p(class="medium-articles-headline") Medium Articles
     .articles-container
-      +article('hello', 'world')
+      +article('http://localhost:8080/assets/turing.jpeg', 'https://medium.com/@gness1804/a-retrospective-on-turing-school-b6009908249', 'A Retrospective on Turing School')
       +article('hello', 'world')
       +article('hello', 'world')
 </template>
