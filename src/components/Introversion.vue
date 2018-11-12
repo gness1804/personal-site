0a02e15232f5e8a31fbaa837c2eb9f38a5e47c6c
gness1804 <graham.nessler@gmail.com>
2018-11-11 18:37:33 -0600

    Begin adding text to Introversion page

--- src/components/Introversion.vue
+++ src/components/Introversion.vue
@@ -1,6 +1,19 @@
 <template lang="pug">
   .introversion
-    h2 I am the Introversion page. Check back soon for more!
+    h2(class="introversion-heading") Introversion
+    .introversion-intro-para
+      p(class="introversion-description")
+        | I have always been an introvert at heart. While definitions of "introvert" and "extrovert" vary, the core difference was first captured by Carl Jung,
+        | who argued that "Each person seems to be energized more by either the external world (extraversion) or the internal world (introversion)."
+        | Later thinkers have clarified that the essential difference stems from the degree of energy that one draws from social interactions.
+        | Susan Cain, (...more stuff)
+    .principles
+        p These are my principles...
+        ol(class="principles-ol")
+          li(class="principles-li") Everyone deserves to be heard; every voice matters.
+          li(class="principles-li") The frequency of contributions in a conversation, be it personal or professional, matters far less than the quality of contributions.
+          li(class="principles-li") 
+
 </template>
 
 <script>
