e87afb43ac4f223496141426cb5f7aa79a8f2fa8
gness1804 <graham.nessler@gmail.com>
2019-05-12 15:48:45 -0500

    Create NotFound component and link to router

---
+++ src/components/NotFound.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+    .not-found
+        h2(class="not-found-heading") Oops! Page Not Found.
+</template>
+
+<script>
+  export default {
+    name: 'NotFound',
+  };
+</script>
+
+<style scoped>
+
+</style>
