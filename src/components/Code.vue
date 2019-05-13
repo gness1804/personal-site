dfc019a21ddaaa239d90fc2ae18790ff786cc30e
gness1804 <graham.nessler@gmail.com>
2019-05-12 20:34:58 -0500

    Replace remaining images with Google Drive links and remove image assets

--- src/components/Code.vue
+++ src/components/Code.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   .code
     h2(class="code-heading") Stuff I Built
-    img(src="../assets/github.png"
+    img(src="https://drive.google.com/uc?id=1N4_NO7oif8EOxh5XOZ0_JUDkd7a-Q46A"
     alt="Github logo" class="big-github-img clickable-img" title="Github" v-on:click="openGithub")
     .projects-container(v-if="projects.length > 0")
       ProjectTile(v-for="project of projects" v-bind:key="project.id" v-bind:project="project")
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,11 +4,11 @@
       router-link(to="/")
         h1 {{name}}
       .social-icons-container
-        img(src="../assets/at.png" alt="Email me." v-on:click="openEmail" class="social-icon clickable-img" title="Email")
-        img(src="../assets/github.png" alt="Github logo." v-on:click="openGithub" class="social-icon clickable-img" title="Github")
-        img(src="../assets/twitter.png" alt="Twitter logo." v-on:click="openTwitter" class="social-icon clickable-img" title="Twitter")
-        img(src="../assets/linkedin.png" alt="LinkedIn logo." v-on:click="openLinkedIn" class="social-icon clickable-img" title="LinkedIn")
-        img(src="../assets/medium.png" alt="Medium logo." v-on:click="openMedium" class="social-icon clickable-img" title="Medium")
+        img(src="https://drive.google.com/uc?id=1WOexIas8I2UlsN27lhQLJ1qCWpceQDh0" alt="Email me." v-on:click="openEmail" class="social-icon clickable-img" title="Email")
+        img(src="https://drive.google.com/uc?id=1N4_NO7oif8EOxh5XOZ0_JUDkd7a-Q46A" alt="Github logo." v-on:click="openGithub" class="social-icon clickable-img" title="Github")
+        img(src="https://drive.google.com/uc?id=11Y9mK8QcD7mYeHAOI7a3xBGKMR11D4-J" alt="Twitter logo." v-on:click="openTwitter" class="social-icon clickable-img" title="Twitter")
+        img(src="https://drive.google.com/uc?id=1QjZ_0ZUIu_AP_qxWEYkL4p-UWoESGSGF" alt="LinkedIn logo." v-on:click="openLinkedIn" class="social-icon clickable-img" title="LinkedIn")
+        img(src="https://drive.google.com/uc?id=1LReDJqCCptYC8UWJL0FTtD-w5rLR0uWC" alt="Medium logo." v-on:click="openMedium" class="social-icon clickable-img" title="Medium")
     nav
       HeaderLinks
 </template>
