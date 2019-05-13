627a6984030905cd64d02824a4c0bf13333cf638
gness1804 <graham.nessler@gmail.com>
2019-05-12 20:11:18 -0500

    Add new deploy and prettier check scripts and replace project image asset links with Google Drive Links

--- flow/projects.js
+++ flow/projects.js
@@ -2,13 +2,10 @@
 
 //import { ProjectI } from '../types/interfaces/';
 
-// TODO: change hardcoded source paths to dynamic paths based on dev or prod
-// TODO: replace stock images with real ones
-
 const projects = [
   {
     id: 1,
-    image: 'http://localhost:8080/assets/flash-shopper.png',
+    image: 'https://drive.google.com/uc?id=1XwGLNCs9mGPGyqdzU7A_j3yahL-Cmjad',
     name: 'Flash Shopper',
     description:
       'A grocery shopping and recipes app build in Vue.js, Firebase, and Pug. Integrates grocery shopping with recipes and allows user to connect to external sites such as Instacart and sort by aisle in a brick-and-mortar store.',
@@ -16,14 +13,14 @@ const projects = [
   },
   {
     id: 2,
-    image: 'http://localhost:8080/assets/bash.png',
+    image: 'https://drive.google.com/uc?id=1D7WDZZ7Sml6jseBGkVIDj-Kx-WTLIBXO',
     name: 'Zsh Scripts',
     description: 'Bash scripts to automate various processes.',
     href: 'https://github.com/gness1804/zsh-scripts',
   },
   {
     id: 3,
-    image: 'http://localhost:8080/assets/weathrly.png',
+    image: 'https://drive.google.com/uc?id=1cEIOsl1WoSEjxyLoCD7R1wf7h8VDmDRF',
     name: 'Weathrly 3.0',
     description:
       "A basic weather app using the Weather Underground API. The user can select any US city and state and display a forecast of several days; select from a convenient dropdown of states; and geolocate (if the user enables this option), which auto-populates the city and state fields based on the user's location.",
@@ -31,7 +28,7 @@ const projects = [
   },
   {
     id: 4,
-    image: 'http://localhost:8080/assets/we-the-people.png',
+    image: 'https://drive.google.com/uc?id=1YdUU28r5OVVDGO-3SCzpnSHoORf0L2oi',
     name: 'MyGov (with Ian Lancaster)',
     description:
       'MyGov is an application created in React Native that allows a user to track current Congressional bills and new laws. Using the Sunlight Congress API, the application brings in data on bills and laws including title, date introduced, date of last action, sponsor, current progress (in House Committee, passed by Senate, enacted...), and other data. The application won first prize in a code competition at Turing School in March 2017.',
